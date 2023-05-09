87d76a3ce56b01d839d4c66525f4be4334e74fac
Nicolas Panozo <nicopanozoc@gmail.com>
2023-05-08 22:09:56 -0400

    cloudwatch funcionando v1

--- main.tf
+++ main.tf
@@ -1,2 +1,21 @@
-# Proyecto Final Nicolas Panozo 
-# CloudWatch Metrics
+# Crear una instancia de EC2
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-06200033affd5e04f"
+  tags = {
+    Name = "ec2-example-instance1"
+  }
+}
+resource "aws_cloudwatch_metric_alarm" "foobar" {
+  alarm_name                = "terraform-test-foobar5"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 2
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = 120
+  statistic                 = "Average"
+  threshold                 = 70
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+}
